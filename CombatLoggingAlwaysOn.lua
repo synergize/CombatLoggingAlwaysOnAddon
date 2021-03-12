e8810ba2abd5a0b10d5fb363dd0c4e8565555bba
Von Trinks <err0r142@gmail.com>
2021-03-11 23:24:15 -0800

    inital working stage

---
+++ CombatLoggingAlwaysOn.lua
@@ -0,0 +1,19 @@
+﻿CombatLoggingAlwaysOnConfig = 
+{
+	frame = CreateFrame("Frame", "frmCombatLoggingAlwaysOn")
+}
+CombatLoggingAlwaysOnConfig.frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+CombatLoggingAlwaysOnConfig.frame:SetScript("OnEvent", function(self, event, ...)
+
+	if (event == "PLAYER_ENTERING_WORLD") then
+		isInitialLogin, isReloadingUi = ...
+		if (isInitialLogin) then 
+			if not (LoggingCombat()) then
+				DEFAULT_CHAT_FRAME:AddMessage("Combat is not being logged - starting it!", 0.0, 1.0, 0.0);  
+				LoggingCombat(1);
+			end
+		end	
+	end
+	
+end)
